1f141044f4938c804dd9ceff7c885eb37c7657ea
Vivimsa <vitoria.gomes@engesoftware.com.br>
2025-11-08 14:58:45 -0300

    primeiro commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
-
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-//Route::post('/login',[UserController::class,'store']);
