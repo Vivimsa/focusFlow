ecd102c68c167700d63d1c984851144512cefb6c
Vivimsa <vitoria.gomes@engesoftware.com.br>
2025-11-08 14:41:43 -0300

    primeiro commit

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    var_dump($request);
     return $request->user();
 })->middleware('auth:sanctum');
