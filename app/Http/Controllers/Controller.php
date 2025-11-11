b5e9cb982cac1ffd2a897985db5583633394e79d
Vitória Maria de Souza Almeida Gomes <vitoria.gomes@engesoftware.com.br>
2025-11-11 11:35:18 -0300

    Configuração de Swagger para documentação

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ *     title="My First API Documentation",
+ *     version="0.1",
+ *      @OA\Contact(
+ *          email="info@yeagger.com"
+ *      ),
+ * ),
+ *  @OA\Server(
+ *      description="Learning env",
+ *      url="https://foo.localhost:8000/api/"
+ *  ),
+ */
 abstract class Controller
 {
 
